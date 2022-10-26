e4371233fa79dbd0784311059fd8d2028f290f85
RafaelRochaF <rafelrochaf@gmail.com>
2022-10-26 19:34:58 -0300

    25-10-22

--- 25-10-22/Exercicio1-3.py
+++ 25-10-22/Exercicio1-3.py
@@ -1,5 +1,17 @@
 a = input("digite um numero:")
 b = input("digite um segundo número:")
 soma = (int(a)+int(b))
+print("a soma dos números" +str(a)+ "+" +str(b)+ "é:")
 print(soma)
 
+subtracao = (int(a)-int(b))
+print("a subtração dos números" +str(a)+ "+" +str(b)+ "é:")
+print(subtracao)
+
+multiplicacao = (int(a)*int(b))
+print("a multiplicação dos números" +str(a)+ "+" +str(b)+ "é:")
+print(multiplicacao)
+
+divisao = (int(a)/int(b))
+print("a divisão dos números" +str(a)+ "+" +str(b)+ "é:")
+print(divisao)
