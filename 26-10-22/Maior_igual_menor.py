9ed5d6e06247d197397e6803a47b4f971b7dea6d
RafaelRochaF <rafelrochaf@gmail.com>
2022-10-26 21:20:08 -0300

    26-10-22

---
+++ 26-10-22/Maior_igual_menor.py
@@ -0,0 +1,9 @@
+N1 = int(input("Digite um numero inteiro: "))
+if N1 > 10:
+    print("O valor digitado é maior que 10")
+
+elif N1 < 10:
+    print("O valor digitado é menor que 10")
+
+else:
+    print("O valor digitado é igual a 10")
