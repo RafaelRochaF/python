56bfadf0bdb735bbfebdf7a1d0830a1504fd7372
RafaelRochaF <rafelrochaf@gmail.com>
2022-10-27 21:51:17 -0300

    27-10-22

---
+++ 27-10-22/Atividade_Enquanto.py
@@ -0,0 +1,21 @@
+# Atividade Calculadora
+resposta = ("s" or "n")
+while resposta == "s":
+   v1 = int(input("Digite um valor inteiro: "))
+   sinal = input("Digite =, - ,* ou /: ")
+   v2 = int(input("Digite um segundo valor inteiro para a conta: "))
+
+   if sinal == "+" :
+       print("A soma de " ,v1, "+", v2, "é: ", v1+v2)
+   elif sinal == "-" :
+       print("A subtração de " ,v1, "-", v2, "é: ", v1-v2)
+   elif sinal == "*" :
+       print("A multiplicação de " ,v1, "*", v2, "é: ", v1*v2)
+   elif sinal == "/":
+       print("A divisão de ", v1, "/", v2, "é: ", v1/v2)
+   else:
+       print("valor invalido")
+
+   resposta = input("Deseja continuar a usar a calculadora? s/n")
+
+
