2352d46ec7140f7c0bd2ddcc3ce7d7153801fa5b
RafaelRochaF <rafelrochaf@gmail.com>
2022-10-31 19:19:13 -0300

    28-10-22

--- 28-10-22/Notas_1.py
+++ 28-10-22/Notas_1.py
@@ -1,15 +1,18 @@
-bim1 = float(input("Digite a nota do primeiro bimestre: "))
-bim2 = float(input("Digite a nota do segundo bimestre: "))
-bim3 = float(input("Digite a nota do terceiro bimestre: "))
-bim4 = float(input("Digite a nota do quarto bimestre: "))
+bim1 = float(input("Digite a nota do primeiro bimestre (Valor máximo de 25pts) : "))
+bim2 = float(input("Digite a nota do segundo bimestre (Valor máximo de 25pts) : "))
+bim3 = float(input("Digite a nota do terceiro bimestre (Valor máximo de 25pts) : "))
+bim4 = float(input("Digite a nota do quarto bimestre (Valor máximo de 25pts) : "))
 
 resultado = bim1+bim2+bim3+bim4
 
-if resultado >= 60:
+if resultado >=60 and resultado<=100:
     print("O aluno foi APROVADO com ", resultado, " pontos")
 
-elif resultado >=40 and 60:
+elif resultado >=40 and resultado <=60:
     print("O aluno está de recuperação com ", resultado, " pontos")
 
-else:
+elif resultado <40 and resultado >=0:
     print("O aluno foi reprovado com ", resultado, " pontos")
+
+else:
+    print("Valor invalido, confirme os valores digitados!!!")
