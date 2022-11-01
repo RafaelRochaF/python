853a4164f5d114ba302fc15a31fa48e63f10ed8b
RafaelRochaF <rafelrochaf@gmail.com>
2022-11-01 19:11:12 -0300

    31-10-22

--- 31-10-22/FuncaoMultiplicar.py
+++ 31-10-22/FuncaoMultiplicar.py
@@ -1,11 +1,12 @@
+#Função de multiplicação
 
-
-def multiplecacao(n1,n2):
+def multiplicacao(n1,n2):
     resultado = n1*n2
 
     return resultado
 
 
-n1 = float(input("Digite um valor: "))
-n2 = float(input("Digite um valor para ser multiplicado pelo primeiro: "))
-
+n1 = float(input("Digite um valor real ou inteiro: "))
+n2 = float(input("Digite um valor real ou inteiro para ser multiplicado pelo primeiro: "))
+resultado = multiplicacao(n1, n2)
+print("A multiplicação dos números ",n1,"*",n2,"=",resultado)
